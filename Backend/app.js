845d984af8b7c150622eac65314f064ddd427e09
Manohar jinka <manujinka22@gmail.com>
2025-07-30 09:33:35 +0530

    updated env

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5000', 
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
     credentials: true,              
 }));
 
